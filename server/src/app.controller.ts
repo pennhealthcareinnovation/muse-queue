143c486a8ba82158bca05a64c20eb7474a137b8d
Emeka C. Anyanwu <emeka@emeka.ca>
2023-03-10 11:52:15 -0500

    debug logging endpoint

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,13 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiExcludeController } from '@nestjs/swagger';
+import { Body, Controller, Get, Logger, Post } from '@nestjs/common';
+import { ApiExcludeController, ApiExcludeEndpoint, ApiTags } from '@nestjs/swagger';
 
-@ApiExcludeController()
+@ApiTags('Base')
 @Controller()
 export class AppController {
   constructor() { }
 
+  private readonly logger = new Logger(AppController.name);
+
+  @ApiExcludeEndpoint()
   @Get()
   getHello(): string {
     return 'Muse Queue';
+  }
+
+  @Post('/api/debug')
+  async debug(
+    @Body() body: any
+  ) {
+    this.logger.debug(body)
   }
 }
